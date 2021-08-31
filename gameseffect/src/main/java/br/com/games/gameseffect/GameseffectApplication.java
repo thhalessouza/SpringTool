185153362e9aca72e16e103e97916d0c77fc44da
Thales Souza <88193323+thhalessouza@users.noreply.github.com>
2021-08-30 23:00:23 -0300

    Add files via upload

---
+++ gameseffect/src/main/java/br/com/games/gameseffect/GameseffectApplication.java
@@ -0,0 +1,13 @@
+package br.com.games.gameseffect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameseffectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameseffectApplication.class, args);
+	}
+
+}
